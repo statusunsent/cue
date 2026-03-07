947467ddb42903699ee82819a936df20169cf8b4
a <a@as-Virtual-Machine.local>
2026-03-07 14:42:31 -0800

    Add device-aware initialization to cues.clj

--- src/core.clj
+++ src/core.clj
@@ -1,9 +1,16 @@
 (ns core
   (:require
    [babashka.fs :refer [file]]
-   [libpython-clj2.python :refer [initialize!]]))
+   [libpython-clj2.python :refer [$a from-import initialize!]]))
 
 (initialize!)
+
+(from-import torch cuda device)
+
+(def device*
+  (device (if ($a cuda is_available)
+            "cuda"
+            "cpu")))
 
 (def data-directory
   "data")
--- src/cues.clj
+++ src/cues.clj
@@ -1,12 +1,19 @@
 (ns cues
   (:require [core :refer [candidates-file]]
             [lambdaisland.edn-lines :as edn-lines]
-            [libpython-clj2.python :refer [from-import]]))
+            [libpython-clj2.python :refer [$a ->py-list from-import]]))
 
 (def candidates
   (edn-lines/slurp candidates-file))
 
 (from-import sentence_transformers SentenceTransformer)
 
+(from-import torch cuda device inference_mode nn nonzero tensor)
+
+(def device*
+  (device (if ($a cuda is_available)
+            "cuda"
+            "cpu")))
+
 (def model
-  (SentenceTransformer "Qwen/Qwen3-Embedding-0.6B"))
+  (SentenceTransformer "Qwen/Qwen3-Embedding-0.6B" :device device*))
