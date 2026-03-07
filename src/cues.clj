e01016876367f3e961b69be73a7fbb95be83149c
a <a@as-Virtual-Machine.local>
2026-03-07 14:21:56 -0800

    Import SentenceTransformer using from-import

--- src/cues.clj
+++ src/cues.clj
@@ -1,6 +1,9 @@
 (ns cues
   (:require [core :refer [candidates-file]]
-            [lambdaisland.edn-lines :as edn-lines]))
+            [lambdaisland.edn-lines :as edn-lines]
+            [libpython-clj2.python :refer [from-import]]))
 
 (def candidates
   (edn-lines/slurp candidates-file))
+
+(from-import sentence_transformers SentenceTransformer)
