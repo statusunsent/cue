493d0896555b3e3ea1192a65ce76063c20c7de66
a <a@as-Virtual-Machine.local>
2026-03-12 23:55:05 -0700

    Update test to reflect revised cleaned string output

--- test/cues_test.clj
+++ test/cues_test.clj
@@ -8,6 +8,6 @@
            clean
            count
            (= 1)))
-  (is (= ["Hello world." -1] (last (clean [["  Hello world. This should be gone." -1]]))))
+  (is (= ["Hello." -1] (last (clean [["  Hello. This should be gone." -1]]))))
   (is (= 1 (count (clean [["!!" -1]]))))
   (is (= ["你好." -1] (last (clean [["你好." -1]])))))
