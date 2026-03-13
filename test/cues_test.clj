ceba5cf80938742cd57d60247797684099f98905
a <a@as-Virtual-Machine.local>
2026-03-12 23:51:22 -0700

    Extend clean function tests to include non‑ASCII sentence handling

--- test/cues_test.clj
+++ test/cues_test.clj
@@ -9,4 +9,5 @@
            count
            (= 1)))
   (is (= ["Hello world." -1] (last (clean [["  Hello world. This should be gone." -1]]))))
-  (is (= 1 (count (clean [["!!" -1]])))))
+  (is (= 1 (count (clean [["!!" -1]]))))
+  (is (= ["你好." -1] (last (clean [["你好." -1]])))))
